6ce2f0121a20ff51c8e8f13223d50d62c1a37a8e
HIRONORI KIKUCHI <hironori.kikuchi.pgm@gmail.com>
2020-03-19 22:54:43 +0700

    add: ts config setting
    outDir setting
    rootDir setting
    additional library setting
    source map setting
    remove comments setting
    no emit on error setting
    additional check setting
    exclude file setting

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-console.log('this is the beginning...');
+const button = document.querySelector('button')!; // [!] ensure that button object is exists
+
+// error: noImplicitReturns
+// function addNums(n1: number, n2: number) {
+//   if (n1 + n2 > 0) {
+//     return n1 + n2;
+//   }
+// }
+
+function clickHandler(message: string) {
+  // let userName = 'Max'; // error no used local variable
+  console.log(message);
+}
+
+if (button) {
+  button.addEventListener('click', clickHandler.bind(null, 'Welcome!'));
+}
