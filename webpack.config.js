bc8ea2394033d4abae9604a0b08c6ebff45a9bbd
HIRONORI KIKUCHI <hironori.kikuchi.pgm@gmail.com>
2020-03-23 00:15:25 +0700

    refactor: folder structure and build setting
    use webpack to build typescript and js
    update gitignore setting
    update lite-server's rootDir setting

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+// TODO: reduce the file size to recommended size limit
+module.exports = {
+  entry: './src/app.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
